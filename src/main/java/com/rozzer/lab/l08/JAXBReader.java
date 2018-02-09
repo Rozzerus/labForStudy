aabd271ae760c9f5e290a3d29909f38c82ab18f4
ZaharovRK <psbhackathon014@yandex.ru>
2018-02-09 23:56:15 +0400

    lab 8

--- src/main/java/com/rozzer/lab/l08/JAXBReader.java
+++ src/main/java/com/rozzer/lab/l08/JAXBReader.java
@@ -4,6 +4,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.util.List;
 
 public class JAXBReader {
 
@@ -16,12 +17,36 @@ public class JAXBReader {
             Unmarshaller jaxbUnmarshaller = null;
             jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             Bookcase bookcase = (Bookcase) jaxbUnmarshaller.unmarshal(file);
-            Bookshelf bookshelf = bookcase.getBookshelfs().iterator().next();
-            String value = bookshelf.getBooks().iterator().next().getPages().getValue();
-            System.out.println(value);
+            validatePagesSum(bookcase);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void validatePagesSum(Bookcase bookcase){
+        bookcase.getBookshelfs().forEach(bookshelf ->
+            bookshelf.getBooks().forEach(JAXBReader::validate));
+    }
+
+    private static void validate(Book book){
+        String sum = String.valueOf(count(book.getChapters(), book.getIntro()));
+        Pages bookPages = book.getPages();
+        if (bookPages != null) {
+            if (sum.equals(bookPages.getValue()))
+                bookPages.setValue(sum);
+        } else {
+            bookPages = new Pages();
+            bookPages.setValue(sum);
+            book.setPages(bookPages);
+        }
+    }
+
+    private static int count(List<Chapter> chapters, Intro intro){
+        int sum = 0;
+        sum+=Integer.valueOf(intro.getPages());
+        for (Chapter cur: chapters)
+            sum+=Integer.valueOf(cur.getPages());
+        return sum;
     }
 
 }
